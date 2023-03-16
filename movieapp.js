8cc75d75aded180f60df11e48e25632a51a1f1aa
Abhishek Darwekar <117111036+Abhishek280999@users.noreply.github.com>
2023-03-16 17:58:55 +0530

    change

--- movieapp.js
+++ movieapp.js
@@ -4,7 +4,7 @@ var typed = new Typed("header>span", {
     backSpeed:200,
     loop: true
   })
-
+popularMovies()
 function popularMovies() {
   let chge = Math.round(Math.random() * 50);
   fetch(
@@ -51,74 +51,74 @@ function popularMovies_append(item) {
   });
 }
 
-popularMovies();
+  let wait;
+  //when we type something in input then it invokes debounce function & after it settimeout start and it delay displayMovie fun
+  //after delay over it invokes displayMovies fun
+  function debounce(func, delay) {
+    clearTimeout(wait);
+    wait = setTimeout(function () {
+      func();
+    }, delay);
+  }
 
-function getdetails(item) {
-  localStorage.setItem("MovieDetails", JSON.stringify(item));
-  window.location.href = "Moviedetails.html";
-}
 
-let bomb;
-
-function debounce(func, delay) {
-  clearTimeout(bomb);
-  bomb = setTimeout(function () {
-    func();
-  }, delay);
-}
-
-async function Movie_Search() {
-  try {
-    let input = document.getElementById("name").value;
-    console.log(input);
-    let res = fetch(
+  async function getMovie() {
+    try {
+      let input = document.getElementById("name").value;
+      let res = await fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=10ac63d1d504ae240549f06c6cc417b4&query=${input}`
-    );
-    let data =  res.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  } finally {
-    console.log("worked finally");
+      );
+      let data = await res.json();
+      return data;
+    } catch (err) {
+      console.log(err);
+    } finally {
+      console.log("worked finally");
+    }
   }
-}
+   
+   async function displayMovie() {
+    let movie_data = await getMovie();
 
- function displayMovie() {
-  let movie_data =  Movie_Search();
-
-  if (movie_data === undefined) {
-    return false;
+    if (movie_data === undefined) {
+      return false;
+    }
+    
+    showSearchMovies(movie_data.results);
+  }
+  
+  function showSearchMovies(result){
+    document.getElementById("searchResult").innerHTML=""
+    document.getElementById("searchResult").style.display="block"
+    //console.log(result)
+    result.forEach(function(item){
+      let div = document.createElement("div")
+      div.setAttribute("id","searchdiv")
+      
+      div.addEventListener("click", function(){
+        getdetails(item)
+      })
+      let imgdiv = document.createElement("div")
+      imgdiv.setAttribute("class","imgdiv")
+      let img = document.createElement("img")
+      img.src =  `https://image.tmdb.org/t/p/original/${item.poster_path}`;
+      let div1 = document.createElement("div")
+      div1.setAttribute("id","searchdiv1")
+      let title1 = document.createElement("h3");
+      title1.textContent = item.title;
+      let year1 = document.createElement("h5");
+      year1.textContent = item.release_date;
+      year1.style.margin = "10px"
+      
+      imgdiv.append(img)
+      div1.append(title1,year1)
+      div.append(imgdiv,div1)
+      document.getElementById("searchResult").append(div)
+    })
+  }
+  function getdetails(item){
+   
+    localStorage.setItem("MovieDetails", JSON.stringify(item))
+    window.location.href = "Moviedetails.html"
   }
 
-  showSearchMovies(movie_data.results);
-}
-
-function showSearchMovies(result) {
-  document.getElementById("searchResult").innerHTML = "";
-  document.getElementById("searchResult").style.display = "block";
-  //console.log(result)
-  result.forEach(function (item) {
-    let div = document.createElement("div");
-    div.setAttribute("id", "searchdiv");
-
-    div.addEventListener("click", function () {
-      getdetails(item);
-    });
-    let imgdiv = document.createElement("div");
-    imgdiv.setAttribute("class", "imgdiv");
-    let img = document.createElement("img");
-    img.src = `https://image.tmdb.org/t/p/original/${item.poster_path}`;
-    let div1 = document.createElement("div");
-    div1.setAttribute("id", "searchdiv1");
-    let title1 = document.createElement("h3");
-    title1.textContent = item.title;
-    let year1 = document.createElement("h5");
-    year1.textContent = item.release_date;
-    year1.style.margin = "10px";
-
-    imgdiv.append(img);
-    div1.append(title1, year1);
-    div.append(imgdiv, div1);
-    document.getElementById("searchResult").append(div);
-  });
-}
